95482c351a1c99534af6ebe3767e9401d9f3dea1
KUNAL FARMAH <kunalfarmah98@gmail.com>
2020-05-21 00:17:55 +0530

    Added optional login to get all items

--- Resources/item.py
+++ Resources/item.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import Flask,request
 from flask_restful import Resource, Api, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 
 from Models.item import ItemModel
 
@@ -85,6 +85,16 @@ class Item (Resource):
 
 
 class ItemList(Resource):
+    # adding option auth to get complete info
+    # give limited data for logged out users
+    @jwt_optional
     def get(self):
-        return {'items':[item.json() for item in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items':items},200
+        
+        return {'items': [item['name'] for item in items],
+                'message': 'Login or provide authorization header to get detailed item information'
+                },200
 
